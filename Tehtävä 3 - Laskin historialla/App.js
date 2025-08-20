d90a1eee4383aa3cb789a5998652358f7f99527e
Jani Luuppala <jani.luuppala@outlook.com>
2025-08-20 15:35:18 +0300

    Tehtävä 3 valmis

---
+++ Tehtävä 3 - Laskin historialla/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NumberInput } from './src/components';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <NumberInput />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ Tehtävä 3 - Laskin historialla/src/components/NumberInput.js
@@ -0,0 +1,133 @@
+import { StyleSheet, Text, View, TextInput, Pressable, TouchableWithoutFeedback, Keyboard, FlatList } from 'react-native';
+import { useState } from 'react';
+
+function NumberInput() {
+    const [numberOne, setNumberOne] = useState("");
+    const [numberTwo, setNumberTwo] = useState("");
+    const [numbersTotal, setNumbersTotal] = useState("");
+    const [history, setHistory] = useState([]);
+    
+    const PlusButton = () => {
+        return (
+            <Pressable
+                style={({ pressed }) => [
+                    styles.buttons,
+                    {backgroundColor: pressed ? 'darkgreen' : 'green' },
+                ]}
+                onPress={() => {
+                    const result = Number(numberOne) + Number(numberTwo);
+                    setNumbersTotal(result);
+                    const entry = numberOne + " + " + numberTwo + " = " + result;
+                    setHistory([...history, { value: entry }]);
+                }}
+            >
+                <Text style={styles.buttonText}>+</Text>
+            </Pressable>
+        );
+    };
+
+    const MinusButton = () => {
+        return (
+            <Pressable
+                style={({ pressed }) => [
+                    styles.buttons,
+                    {backgroundColor: pressed ? 'darkred' : 'red' },
+                ]}
+                onPress={() => {
+                    const result = Number(numberOne) - Number(numberTwo);
+                    setNumbersTotal(result);
+                    const entry = numberOne + " - " + numberTwo + " = " + result;
+                    setHistory([...history, { value: entry }]);
+                }}
+            >
+                <Text style={styles.buttonText}>-</Text>
+            </Pressable>
+        );
+    };
+
+    return (
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View>
+            <View style={styles.container}></View>
+            <View style={styles.containercolumn}>
+                <TextInput
+                    placeholder='Enter number one'
+                    keyboardType="number-pad"
+                    style={styles.textinput}
+                    onChangeText={numberOne => setNumberOne(numberOne)}
+                    value={numberOne}
+                />
+                <TextInput
+                    placeholder='Enter number two'
+                    keyboardType="number-pad"
+                    style={styles.textinput}
+                    onChangeText={numberTwo => setNumberTwo(numberTwo)}
+                    value={numberTwo}
+                />
+    
+                <Text>Result: {numbersTotal}</Text>
+            </View>
+            <View style={styles.containerrow}>
+                <PlusButton />
+                <MinusButton />
+            </View>
+            <View style={styles.container}>
+                <FlatList 
+                    data={history} 
+                    renderItem={({item}) => <Text>{item.value}</Text>} 
+                    keyExtractor={(_, index) => index.toString()} 
+                />
+            </View>
+            <View style={styles.container}></View>
+            <View style={styles.container}></View>
+        </View>
+        </TouchableWithoutFeedback>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textinput: {
+        backgroundColor: '#e8e4e4c5',
+        borderColor: '#000',
+        borderWidth: 1,
+        width: 200,
+        height: 40,
+        margin: 10,
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttons: {
+        borderColor: '#000',
+        borderWidth: 1,
+        width: 50,
+        height: 50,
+        margin: 10,
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+    containerrow: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containercolumn: {
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default NumberInput;
