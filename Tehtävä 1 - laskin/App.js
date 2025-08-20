6b49c99e121a6499738c00482fe2abf0b99354c0
Jani Luuppala <jani.luuppala@outlook.com>
2025-08-20 12:39:28 +0300

    Add folder as regular directory

---
+++ Tehtävä 1 - laskin/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import { NumberInput } from './components'
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <NumberInput />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
